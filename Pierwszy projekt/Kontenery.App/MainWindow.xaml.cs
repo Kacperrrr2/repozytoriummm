425aa87251bcf714be2dfbea1c3dc0269deeb62d
Test <test@test.pl>
2023-09-12 12:57:46 +0200

    new changing

---
+++ Pierwszy projekt/Kontenery.App/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kontenery.App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonWrapPanel_Click(object sender, RoutedEventArgs e)
+        {
+            WrapPanelWindow wrapPanelWindow = new WrapPanelWindow();
+            wrapPanelWindow.Show();
+        }
+
+        private void ButtonStackPanel_Click(object sender, RoutedEventArgs e)
+        {
+            StackPanelWindow stackPanelWindow = new StackPanelWindow();
+            stackPanelWindow.Show();
+
+        }
+
+
+        private void ButtonDockPanel_Click(object sender, RoutedEventArgs e)
+        {
+            DockPanelWindow DockPanelWindow = new DockPanelWindow();
+            DockPanelWindow.Show();
+
+        }
+        private void ButtonGridPanel_Click(object sender, RoutedEventArgs e)
+        {
+            GridPanelWindow GridPanelWindow = new GridPanelWindow();
+            GridPanelWindow.Show();
+
+        }
+
+
+    }
+}
